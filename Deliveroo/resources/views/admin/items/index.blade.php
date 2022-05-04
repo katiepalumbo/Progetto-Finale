1c3c1812e6029191e4adcf449f44a69c11dacf1b
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-05-04 18:25:20 +0200

    validation

--- Deliveroo/resources/views/admin/items/index.blade.php
+++ Deliveroo/resources/views/admin/items/index.blade.php
@@ -8,7 +8,7 @@
                 <table class="table">
 
                     <a href="{{route('admin.items.create')}}" class="btn btn-warning mb-3">Crea nuovo piatto</a>
-                    
+
                     <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -16,8 +16,9 @@
                         <th scope="col">Descrizione</th>
                         <th scope="col">Slug</th>
                         <th scope="col">Categoria</th>
-                        <th scope="col">Prezzo</th>
                         <th scope="col">Opzioni</th>
+                        <th scope="col">Visibile</th>
+                        <th scope="col">Prezzo</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -29,19 +30,26 @@
                                 <td>{{$item->slug}}</td>
                                 <td>{{isset($item->category)?$item->category->name:'N.D.'}}</td>
                                 <td>€{{$item->price}}&nbsp</td>
-                                <td class="d-flex">    
+
+                                @if ($item->visible==1)
+                                    <td>yes</td>
+                                @else
+                                    <td>no</td>
+                                @endif
+
+                                <td class="d-flex">
                                     <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary m-2">Visualizza</a>
                                     <a href="{{route('admin.items.edit', $item->id)}}" class="btn btn-secondary m-2">Modifica</a>
 
                                     <form method="POST" action="{{route('admin.items.destroy', $item->id)}}">
-                                    
+
                                             @csrf
                                             @method('DELETE')
 
                                             <button type="submit" class="btn btn-danger m-2">
-                                                ELIMINA
+                                                Elimina
                                             </button>
-                                    
+
                                     </form>
                                 </td>
                             </tr>
