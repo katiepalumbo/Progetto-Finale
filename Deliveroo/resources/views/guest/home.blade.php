284c1d95c77bcd0d2fe834bb38e58013ebd288ee
marco maione <maionemarco14@gmail.com>
2022-05-06 12:59:37 +0200

    corezzione url non riuscita creare Api

--- Deliveroo/resources/views/guest/home.blade.php
+++ Deliveroo/resources/views/guest/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<body>
 
-                    {{ __('/ADMIN') }}
-                </div>
-            </div>
-        </div>
+    <div id="root">
+
     </div>
-</div>
-@endsection
+
+    <script src="{{asset('js/front.js')}}"></script>
+
+</body>
+
+</html>
+
