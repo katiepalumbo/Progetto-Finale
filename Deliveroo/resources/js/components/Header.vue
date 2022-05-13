ad45ff9642e16aa853514aaafad9cd2798535497
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-05-13 14:03:26 +0200

    Inizio stilizzazione home

--- Deliveroo/resources/js/components/Header.vue
+++ Deliveroo/resources/js/components/Header.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="container border-2 rounded bg-dark">
+    <div class="container-fluid container-header">
         <div class="row">
             <div class="col-10 offset-1">
-                <nav class="py-2">
-                    <ul class="nav justify-content-center">
+                <nav class="py-2 box-nav">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <router-link class="text-white nav-link logo-header" aria-current="page" to="">Deliveboo</router-link>
+                        </li>
+                    </ul>
+                    <ul class="nav">
                         <li v-for="(link, index) in navLinks" :key="index" class="nav-item">
-                            <router-link class="text-white nav-link" aria-current="page" :to="{name: link.to}">{{ link.name }}</router-link>
+                            <router-link class="text-white btn-header" aria-current="page" :to="{name: link.to}">{{ link.name }}</router-link>
                         </li>
                         <li>
-                            <a class="nav-link" href="/admin">Accedi o Registrati</a>
+                            <a class=" btn-header" href="/admin">Accedi o Registrati</a>
                         </li>
                     </ul>
                 </nav>
@@ -31,10 +36,6 @@ export default {
                     to : 'checkout',
                     name : 'Carrello',
                 },
-                // {
-                //     to : 'contacts',
-                //     name : 'Contattaci',
-                // },
                 {
                     to : 'menu',
                     name : 'Menù',
@@ -45,6 +46,41 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
-</style>
+    .container-header {
+        background-color: #FF4718;
+
+        .box-nav {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+
+            .logo-header {
+                font-size: 21px;
+            }
+        }
+
+        .btn-header {
+            display: inline-block;
+            background-color: #9538f2;
+            padding: 6px 16px;
+            text-decoration: none;
+            font-weight: bold;
+            color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 4px 0 #7633b9;
+            transition: all 0.3s;
+            margin-left: 10px;
+        }
+
+        .btn-header:hover {
+            transform: translateY(2px);
+            box-shadow: 0 2px 0 #9538f2;
+        }
+    }
+
+</style>>
+
+
--- Deliveroo/resources/js/pages/Home.vue
+++ Deliveroo/resources/js/pages/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-      <Main/>
+      <Jumbo/>
   </div>
 </template>
 
 <script>
+import Jumbo from '../components/JumboHome.vue'
 import Main from '../components/Main.vue'
 export default {
     name: 'Home',
     components: {
       Main,
+      Jumbo
     }
 }
 </script>
