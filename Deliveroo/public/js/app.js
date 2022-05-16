348a8b432aa5fe5ba9b39b4901087dfcc5b7ae7c
Jomm <venturini.thomas95@gmail.com>
2022-05-16 02:53:46 +0200

    login completato

--- Deliveroo/public/js/app.js
+++ Deliveroo/public/js/app.js
@@ -53745,6 +53745,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/admin.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/admin.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -53756,27 +53767,16 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/login.scss":
-/*!***********************************!*\
-  !*** ./resources/sass/login.scss ***!
-  \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'C:\\progLaravel\\Progetto-Finale\\Deliveroo\\resources\\sass\\login.scss'\n    at C:\\progLaravel\\Progetto-Finale\\Deliveroo\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\progLaravel\\Progetto-Finale\\Deliveroo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\progLaravel\\Progetto-Finale\\Deliveroo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at C:\\progLaravel\\Progetto-Finale\\Deliveroo\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (node:internal/process/task_queues:78:11)");
-
-/***/ }),
-
 /***/ 0:
 /*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/login.scss ***!
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/admin.scss ***!
   \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\progLaravel\Progetto-Finale\Deliveroo\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\progLaravel\Progetto-Finale\Deliveroo\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\progLaravel\Progetto-Finale\Deliveroo\resources\sass\login.scss */"./resources/sass/login.scss");
+__webpack_require__(/*! C:\Users\Tonno\Desktop\Boolean\Deliveboo\Progetto-Finale\Deliveroo\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\Tonno\Desktop\Boolean\Deliveboo\Progetto-Finale\Deliveroo\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\Tonno\Desktop\Boolean\Deliveboo\Progetto-Finale\Deliveroo\resources\sass\admin.scss */"./resources/sass/admin.scss");
 
 
 /***/ })
---
+++ Deliveroo/resources/views/layouts/app2.blade.php
@@ -0,0 +1,32 @@
+{{-- app2.blade.php extend per la pagina di login, per non avere la navbar e i
+    il padding superiore --}}
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Login</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+    
+</head>
+<body>
+    <main class="">
+
+        @yield('content')
+
+    </main>
+</body>
+</html>
