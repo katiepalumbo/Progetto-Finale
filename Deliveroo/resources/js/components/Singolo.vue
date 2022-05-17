39e7d97db67f1d0624b9a0f044cee2c44a718ca9
katiepalumbo <palumbo.katie@gmail.com>
2022-05-17 10:00:07 +0200

    piccola correzione css

--- Deliveroo/resources/js/components/Singolo.vue
+++ Deliveroo/resources/js/components/Singolo.vue
@@ -111,11 +111,7 @@ export default {
                 text-transform: uppercase;
                 margin: 6px 4px;
                 color: #fff;
-<<<<<<< HEAD
                 @include title-font;
-=======
-                
->>>>>>> home-style-rifiniture
             }
         }
 
